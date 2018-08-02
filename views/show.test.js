c077c0e4d5a14da821eaf65167aedee46f9080e4
Kieran <kieran.a.gibb@gmail.com>
2018-08-02 13:30:35 +0100

    refactor / clarify test

--- views/show.test.js
+++ views/show.test.js
@@ -8,67 +8,58 @@ const Server = require('./lib/testbot')
 const attachStyles = require('./lib/attachStyles')
 const viewName = 'show'
 const View = require(`./${viewName}`)
+const getContent = require('ssb-msg-content')
 
 const server = Server()
+const scuttle = Scuttle(server)
 
+let params = {
+  name: 'MMT longterm wallet',
+  secret: 'test',
+  recps: [server.createFeed().id, server.createFeed().id],
+  quorum: 2
+}
 
-const rootContent = {type: 'dark-crystal/root', version: '1.0.0', name: 'MMT longterm wallet'}
-server.private.publish(rootContent, [server.id], (err, root) => {
-  console.log(err, root)
-  root.value.content = rootContent // skipping unboxing!
+setTimeout(() => scuttle.share.async.share(params, (err, data) => {
+  const { root, ritual, shards } = data
 
-  const opts = {
+  console.log("PRE-POPULATED DATABASE")
+
+  document.body.appendChild(View({
+    scuttle,
     root,
-    scuttle: Scuttle(server)
-  }
-  document.body.appendChild(View(opts))
+    avatar: () => {},
+    modal: () => {},
+  }))
 
-  setTimeout(() => addShards(root.key, (err, shards) => {
-    setTimeout(() => addRequests(root.key, shards), 1600)
-  }, 800))
-})
+  console.log("VIEW APPENDED")
+
+  setTimeout(() => {
+    pull(
+      pull.values(shards),
+      pull.map(shard => {
+        const { root, recps = [] } = getContent(shard)
+        return {
+          type: 'invite',
+          version: '1',
+          recps: recps,
+          root: root,
+          body: 'gimme gimme gimme'
+        }
+      }),
+      pull.asyncMap((shard, callback) => {
+        scuttle.recover.async.request(root.key, shard.recps, (err, request) => {
+          callback(null, server.private.unbox(request))
+        })
+      }),
+      pull.drain(request => {
+        console.log(request)
+      })
+    )
+
+  }, 800)
+}), 800)
 
 attachStyles([
   `${viewName}.mcss`
 ])
-
-function addShards (root, cb = console.log) {
-  const shards = ['abc-124', 'doop-doop', 'swag-dag']
-  pull(
-    pull.values(shards),
-    pull.map(shard => {
-      return {
-        type: 'dark-crystal/shard', version: '1.0.0', root, shard, recps: [server.id, server.createFeed().id]
-      }
-    }),
-    pull.asyncMap((content, cb) => {
-      server.private.publish(content, content.recps, cb)
-    }),
-    pull.collect((err, data) => {
-      if (err) return cb(err)
-
-      cb(null, data)
-    })
-  )
-}
-
-function addRequests(root, shards, cb = console.log) {
-  pull(
-    pull.values(shards),
-    pull.map(shard => {
-      const { root, recps = [] } = getContent(shard)
-      return {
-        type: 'invite',
-        version: '1',
-        recps: recps,
-        root: root,
-        body: 'gimme gimme gimme'
-      }
-    }),
-    pull.asyncMap((content, cb) => server.private.publish(content, content.recps, cb)),
-    pull.collect((err, data) => {
-      if (err) cb(err)
-      else cb(null, data)
-    })
-  )
-}
