1bd8f2f836f33b8c87fdcf04e0c4ed08ff423bae
Kieran <kieran.a.gibb@gmail.com>
2018-08-06 18:42:37 +0100

    find in each shard - db heavy but might work?

--- views/shards/show.js
+++ views/shards/show.js
@@ -1,35 +1,39 @@
 const pull = require('pull-stream')
-const { h, Value, Struct, computed } = require('mutant')
+const { h, Array: MutantArray, Value, Struct, computed } = require('mutant')
 
 const Recipient = require('../component/recipient')
 const Timestamp = require('../component/timestamp')
 const DarkCrystalRequestNew = require('../requests/new')
 
 module.exports = function DarkCrystalShardShow ({ root, scuttle, modal, avatar, msg }) {
-  const {
-    value: {
-      timestamp,
-      content: {
-        recps = []
-      }
-    }
-  } = msg
-
-  // TODO: base on whether a request exists,
-  // requires request data mapped onto shard data
+  const rootId = root.key
+  const { value: { timestamp, content: { recps = [] } } } = msg
   const state = Struct({
     requesting: Value(false),
-    requested: Value(false)
+    requested: Value(false),
+    request: Value()
   })
+
+
+  pull(
+    scuttle.recover.pull.requests(rootId, { live: true }),
+    pull.filter(m => !m.sync),
+    pull.filter(req => req.value.content.recps.includes(recps[0])),
+    pull.drain(request => {
+      state.request.set(request)
+    })
+  )
+
+  const canRequest = computed(
+    [state.request],
+    (request) => request ? null : DarkCrystalRequestNew({ root, scuttle, modal, state, recps })
+  )
 
   return h('div.shard', [
     h('div.overview', [
       Recipient({ recp: recps[0], avatar }),
       Timestamp({ prefix: 'Sent on', timestamp }),
-      computed(
-        [state.requested],
-        (bool) => bool ? null : DarkCrystalRequestNew({ root, scuttle, modal, state, recps })
-      )
+      canRequest()
     ])
   ])
 }
