d4bb7fcf4750a6cc8dfa68e5c24dfd2284e9a1e2
ameba23 <ameba23@systemli.org>
2018-08-09 19:17:47 +0200

    progress bar maximum should be records.length

--- views/rituals/show.js
+++ views/rituals/show.js
@@ -7,7 +7,7 @@ module.exports = function DarkCrystalRitualShow ({ ritual, shardRecords }) {
   return computed([ritual, shardRecords], (ritual, records) => {
     if (!ritual) return
 
-    const { quorum, recps = [] } = getContent(ritual)
+    const { quorum } = getContent(ritual)
     const hasRequests = records.some(r => r.requests.length > 0)
     const recordsWithReplies = records.filter(r => r.replies.length > 0)
 
@@ -19,7 +19,7 @@ module.exports = function DarkCrystalRitualShow ({ ritual, shardRecords }) {
       when(hasRequests,
         ProgressBar({
           prepend: h('h3', 'Progress'),
-          maximum: recps.length,
+          maximum: records.length,
           middle: quorum,
           title: 'Replies:',
           records: recordsWithReplies
