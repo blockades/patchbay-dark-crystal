14e32d5bc0245c47b8e5ff3146f9a78b45454939
ameba23 <ameba23@systemli.org>
2018-08-09 19:19:17 +0200

    progress bar title shows m-of-n

--- views/component/progress.js
+++ views/component/progress.js
@@ -12,8 +12,7 @@ module.exports = function ProgressBar (opts) {
 
   return computed(records, (records) => {
     const value = Array.isArray(records) ? records.length : 0
-    const titleText = [title, String(value)].filter(isString).join(' ')
-
+    const titleText = [title, String(value), 'of', String(maximum)].filter(isString).join(' ')
     return h('ProgressBar', [
       prepend,
       h('progress', { min: 0, max: maximum, value: value, title: titleText }),
