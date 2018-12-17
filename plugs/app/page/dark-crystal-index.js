7862abf1b149884be2672edcf5e5c5343d3dadab
ameba23 <ameba23@systemli.org>
2018-12-17 22:48:43 +0100

    pass modal

--- plugs/app/page/dark-crystal-index.js
+++ plugs/app/page/dark-crystal-index.js
@@ -67,7 +67,12 @@ exports.create = function (api) {
     return h('section.content', { className: computed(mode, m => m === MINE ? '-active' : '') }, [
       formModal,
       h('button -primary', { 'ev-click': () => formOpen.set(true) }, 'New'),
-      CrystalsIndex({ scuttle, routeTo: api.app.sync.goTo, avatar: api.about.html.avatar })
+      CrystalsIndex({
+        scuttle,
+        routeTo: api.app.sync.goTo,
+        avatar: api.about.html.avatar,
+        modal: api.app.html.modal
+      })
     ])
   }
 
