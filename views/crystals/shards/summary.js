1d5018146ce8de50b53dc33ea5fe226bfa637009
ameba23 <ameba23@systemli.org>
2018-12-17 12:03:07 +0100

    recombining for non forwarded shares uses methods in separate file

--- views/crystals/shards/summary.js
+++ views/crystals/shards/summary.js
@@ -1,6 +1,6 @@
-const { h, when, computed, Value, resolve } = require('mutant')
+const { h, when, computed, Value } = require('mutant')
 const getContent = require('ssb-msg-content')
-const { clipboard } = require('electron')
+const { performRecombine, RecombineModal } = require('./recombine')
 
 const ProgressBar = require('../../component/progress-bar')
 const getRecp = require('../../lib/get-recp')
@@ -47,7 +47,7 @@ module.exports = function DarkCrystalShardsSummary ({ ritual, shardRecords, scut
         h('section.recombine', [
           // h('span', 'Quorum reached!'),
           h('button -primary',
-            { 'ev-click': () => performRecombine(root, state) },
+            { 'ev-click': () => performRecombine(root, scuttle, state) },
             when(state.recombining,
               h('i.fa.fa-spinner.fa-pulse'),
               'Show secret'
@@ -58,56 +58,4 @@ module.exports = function DarkCrystalShardsSummary ({ ritual, shardRecords, scut
       RecombineModal(modal, state)
     ])
   })
-
-  function performRecombine (rootId, { recombining, error, secret, modalOpen }) {
-    recombining.set(true)
-
-    scuttle.recover.async.recombine(rootId, (err, s) => {
-      if (err) error.set(err)
-      else secret.set(s)
-
-      recombining.set(false)
-      modalOpen.set(true)
-    })
-  }
-}
-
-function RecombineModal (modal, state) {
-  const { error, modalOpen, secret } = state
-  const copyBtnVisible = Value(true)
-  const copySecret = () => {
-    copyBtnVisible.set(false)
-    clipboard.writeText(resolve(secret))
-
-    setTimeout(() => copyBtnVisible.set(true), 500)
-  }
-
-  // TODO - extract? (and extract styles)
-  const content = h('DarkCrystalSecret', when(error,
-    [
-      h('h1', [
-        'Error combining shards!!!'
-      ]),
-      h('pre', computed(error, e => (e || '').toString())),
-      h('div.actions', [
-        h('button -subtle', { 'ev-click': () => modalOpen.set(false) }, 'close')
-      ])
-    ],
-    [
-      h('h1', 'Your secret'),
-      h('pre', secret),
-      h('div.actions', [
-        when(copyBtnVisible,
-          h('button -primary', { 'ev-click': copySecret }, [
-            h('i.fa.fa-copy'),
-            'Copy to clipboard'
-          ]),
-          h('button', `(ﾉ´ヮ´)ﾉ*:･ﾟ✧`)
-        ),
-        h('button -subtle', { 'ev-click': () => modalOpen.set(false) }, 'close')
-      ])
-    ])
-  )
-
-  return modal(content, { isOpen: modalOpen })
 }
